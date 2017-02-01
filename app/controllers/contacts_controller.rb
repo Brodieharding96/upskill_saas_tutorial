48ec729cb22c7556281f8b2f68731a250cc1a6f7
Brodie Harding <brodieharding84@gmail.com>
2017-02-01 04:28:11 +0000

    Added form validations to model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,6 +2,7 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+  
   def create
     @contact = Contact.new(contact_params)
     if @contact.save
@@ -12,6 +13,6 @@ class ContactsController < ApplicationController
   end
   private
     def contact_params
-       params.require(:contact).permit(:name, :email, :comments)
+       params.require(:contact).permit(:name, :email, :comment)
     end
 end
