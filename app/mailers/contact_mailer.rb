41cf0d447b4a7a809eca8914e4ee4588dfca2155
Brodie Harding <brodieharding84@gmail.com>
2017-02-03 04:09:28 +0000

    Updated contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'brodieharding84@gmail.com' 
+  default to: 'brodieharding96@gmail.com' 
   
   def contact_email(name, email, body)
     @name = name
