9b8bdaf1114370b65042de5a76d54e133950fd04
Brodie Harding <brodieharding84@gmail.com>
2017-02-04 02:18:25 +0000

    Added bootstrap to registration form, added /sign-up route to new_registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'sign-up', to: 'devise/registrations#new'
+  end
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
